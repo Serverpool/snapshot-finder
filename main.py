71c37787706d0cd13012142175ac8c9032de15a8
Maxim Kirpichev <57616894+chelush@users.noreply.github.com>
2023-10-22 19:13:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ parser = argparse.ArgumentParser(description='Solana snapshot finder')
 parser.add_argument('-t', '--threads-count', default=1000, type=int,
                     help='the number of concurrently running threads that check snapshots for rpc nodes')
 
-parser.add_argument('--max_snapshot_age', default=1300, type=int,
+parser.add_argument('--max_snapshot_age', default=10000, type=int,
                     help='How many slots ago the snapshot was created (in slots)')
 parser.add_argument('--min_download_speed', default=60, type=int,
                     help='Minimum average snapshot download speed in megabytes')
@@ -44,8 +44,6 @@ parser.add_argument('-b', '--blacklist', default='', type=str,
 args = parser.parse_args()
 
 DEFAULT_HEADERS = {"Content-Type": "application/json"}
-RPC = args.rpc_address
-WITH_PRIVATE_RPC = args.with_private_rpc
 MAX_SNAPSHOT_AGE_IN_SLOTS = args.max_snapshot_age
 THREADS_COUNT = args.threads_count
 MIN_DOWNLOAD_SPEED_MB = args.min_download_speed
